4d751df0676555190c5682474fcd9264f64c732d
zoe-c <find.zoec@gmail.com>
2017-06-28 16:25:53 -0400

    IT PRINTS. tackle edit and delete next.

--- app.js
+++ app.js
@@ -16,64 +16,25 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(expressValidator());
 
 
-// you need to create a model that has name: item  attributes: {task: string, complete: boolean (T= complete// F= not complete: aka pending)
-// because sequelizer constantly updates.. and adds a created at and completed time stamp... do i need to define an attribute for the status/ completed part?
-//
-// const item = models.Item.build({
-//    task:'sleep',
-//    status: 'f'
-// });
-// item.save().then(function(newItem) {
-//    console.log(newItem.id);
-// });
-// const todo = models.Todo.build({
-//   task: 'sleep',
-//   status: 'incomplete'
-// });
-
-// todo.save().then(function(newTodo){
-//    console.log(newTodo);
-// });
-
-
-
-
+// Requests
 app.get('/', function(req,res) {
-   res.render('app')
+   models.Todo.findAll().then(function(todos){
+      res.render('app', {Todo: todos});
+   });
 });
 
 app.post("/", function (req,res) {
-   // req.checkBody('task','You forgot to enter a task! Please try again.').notEmpty();
-
-   //Checking for validationErrors (you just set validation above== task must not be empty!)
-   // var errors = req.validationErrors();
-   //   if (errors) {
-   //     res.send( "You forgot to enter a task! Go back and try again.");
-   //   } else {
-         // const addTask = {'task': req.body.task};
          const todo = models.Todo.build({
            task: req.body.task,
            status: 'incomplete'
          });
 
          todo.save().then(function(req, res, next){
-            res.render('app', {'task': task});
+
             next();
          });
-
-
-   // }
+         res.redirect('/');
 });
-
-
-// NEED TO MOVE THE RENDER THE GET.
-// ADD REDIRECT TO THE POST.
-// ON RENDER: do similar to below and instead of console.log>> res.render
-// models.Student.findAll().then(function(students){
-//    console.log(students);
-//    console.log('^Here are our students');
-// })
-
 
 // Port
 app.listen(3000, function(){
